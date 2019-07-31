07d0c3d1f4235eaf0e7bcbddf491bd73cd1b1059
unknown <nathaniel95975@gmail.com>
2019-07-31 11:53:00 -0500

    index.hbs test complete

--- notes-app/routes/index.js
+++ notes-app/routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express"),
+  router = express.Router(),
+  notes = require("../models/notes-memory");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET home page */
+router.get("/", async (req, res, next) => {
+  let keylist = await notes.keylist();
+  let keyPromises = keylist.map(key => {
+    return notes.read(key);
+  });
+  let notelist = await Promise.all(keyPromises);
+  res.render("index", { title: "Notes", notelist });
 });
 
 module.exports = router;
